410ee2896a2d3b6da0ca5281fda6c8d38ed1b2dc
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-18 17:11:37 +0530

    eslint check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 interface homePageProps {}
 
 const HomePage: FC<homePageProps> = () => {
-  return <main>Main</main>;
+  return <main>Home page</main>;
 };
 
 export default HomePage;
