8948ac0dc62880531961919e414b2792dca18e20
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-21 15:13:55 +0530

    basic next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const config = require("./config");
+
+const { domains, deviceSizes } = config.image || {};
+
+const nextConfig = {
+  /* config options here */
+  images: {
+    ...(domains ? { domains } : {}),
+    // ...(imageSizes ? ({ imageSizes }) : {}),
+    ...(deviceSizes ? { deviceSizes } : {}),
+    loader: "default",
+  },
+};
+
+module.exports = nextConfig;
+
+// Don't delete this console log, useful to see the commerce config in site deployments
+if (config.isProduction) {
+  console.info("next.config.js", JSON.stringify(module.exports, null, 2));
+}
