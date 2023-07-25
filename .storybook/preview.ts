52214da86390cb9889f483b3c7227bcff5e8fc49
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-25 16:03:50 +0530

    basic storybook preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+import { create } from "@storybook/theming";
+const config = require("../config");
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +11,48 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+  },
+};
+
+const { deviceSizes, breakpoints } = config.image || {};
+
+const BREAKPOINTS_INT = {
+  xs: breakpoints.xs.maxWidth + 1,
+  sm: breakpoints.sm.maxWidth + 1,
+  md: breakpoints.md.maxWidth + 1,
+  lg: breakpoints.lg.maxWidth + 1,
+  xl: deviceSizes[4],
+};
+
+const customViewports = Object.fromEntries(
+  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+    return [
+      key,
+      {
+        name: key,
+        styles: {
+          width: `${val}px`,
+          height: `${(idx + 5) * 10}vh`,
+        },
+      },
+    ];
+  })
+);
+
+const darkTheme = create({ base: "dark" });
+const lightTheme = create({
+  base: "light",
+  // brandImage: logo.darkLogo
+});
+
+export const parameters = {
+  viewport: { viewports: customViewports },
+  layout: "fullscreen",
+  darkMode: {
+    dark: darkTheme,
+    light: lightTheme,
+    stylePreview: true,
+    current: "dark",
   },
 };
 
