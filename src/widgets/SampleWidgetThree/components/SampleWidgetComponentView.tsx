d8d04acd00eb09638c0a093773e58882890d8e6e
Indrajith C <indrajith.chandran@mozanta.com>
2023-07-25 13:54:54 +0530

    sample component

---
+++ src/widgets/SampleWidgetThree/components/SampleWidgetComponentView.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface SampleWidgetComponentViewProps {}
+
+const SampleWidgetComponentView: FC<SampleWidgetComponentViewProps> = () => {
+  return <div> SampleWidgetComponentView </div>;
+};
+
+export default SampleWidgetComponentView;
--- src/widgets/SampleWidgetThree/index.tsx
+++ src/widgets/SampleWidgetThree/index.tsx
@@ -1,3 +1,6 @@
 import SampleWidgetThree from "@widgets/SampleWidgetThree/containers/SampleWidgetThreeContainer";
+import SampleWidgetComponent from "@widgets/SampleWidgetThree/containers/SampleWidgetComponent";
+
+export { SampleWidgetComponent };
 
 export default SampleWidgetThree;
