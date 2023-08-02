cac83d8ee832f395b5865bc71a71a838aa12cc86
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-02 14:21:56 +0530

    headBar component

---
+++ src/widgets/Header/HeadBar/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface HeadBarContainerProps {}
+
+const HeadBarContainer: FC<HeadBarContainerProps> = ({}) => {
+  return <header>head</header>;
+};
+
+export default HeadBarContainer;
