629f8b39c26c79ba162ac951ad79a063c6a0c96f
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-03 14:58:24 +0530

    config updated

--- postcss.config.js
+++ postcss.config.js
@@ -16,10 +16,11 @@ module.exports = {
     [
       "@fullhuman/postcss-purgecss",
       {
-        content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+        content: ["./src/**/*.{js,jsx,ts,tsx}"],
         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        safelist: ["html", "body"],
+        safelist: ["html", "body", "img"],
       },
     ],
+    "postcss-preset-env",
   ],
 };
