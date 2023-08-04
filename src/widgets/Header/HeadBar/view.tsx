54207e8082fc7a7c5eb73f6da668a25b1d3fbbb8
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-04 18:33:13 +0530

    ui updated

--- src/widgets/Header/HeadBar/view.tsx
+++ src/widgets/Header/HeadBar/view.tsx
@@ -3,6 +3,10 @@ import Link from "@components/Link";
 import Image from "@components/Image";
 
 import IconMenu from "@icons/IconMenu";
+import IconHelp from "@icons/IconHelp";
+import IconCategory from "@icons/IconCategory";
+import IconNotification from "@icons/IconChevronDown";
+import IconChevronDown from "@icons/IconChevronDown";
 
 interface HeadBarViewProps {}
 
@@ -15,8 +19,36 @@ const HeadBarView: FC<HeadBarViewProps> = ({}) => {
             <Image src="/dummy/images/image-svg-172x30.svg" width={172} height={30} alt="logo" />
           </Link>
           <button type="button" className="btn btn-toggler">
-            <IconMenu />
+            <IconMenu className="icon fill-muted" />
           </button>
+        </div>
+        <div className="col-auto">
+          <ul className="header-nav-icons nav">
+            <li className="nav-item">
+              <IconHelp className="icon sm fill-primary" />
+            </li>
+            <li className="nav-item">
+              <div className="subscriptions dropdown">
+                <IconCategory className="icon sm stroke-muted" />
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">
+                <div className="position-relative">
+                  <span className="notification" />
+                  <IconNotification className="icon sm fill-muted" />
+                </div>
+              </a>
+            </li>
+            <li className="nav-item">
+              <div className="d-none d-md-flex ms-5 dropdown show">
+                <a className="navbar-user">
+                  <span>Michael Richard</span>
+                  <IconChevronDown className="icon xs fill-muted ms-1" />
+                </a>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
