c7b59372ac28cbb49544ecc48799c61d3cc0d551
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-06 21:59:36 +0530

    icon added

--- src/widgets/Header/SideBar/view.tsx
+++ src/widgets/Header/SideBar/view.tsx
@@ -1,18 +1,34 @@
 import { FC } from "react";
 
+import Tooltip from "@components/Tooltip";
+
+import IconDashboard from "@icons/IconDashboard";
+
 interface SideBarViewProps {
   isOpen: Boolean;
 }
 
 const SideBarView: FC<SideBarViewProps> = (props) => {
   const { isOpen } = props;
+
+  const dashboardIcon = (
+    <a href="/" className="nav-link active">
+      <IconDashboard className="icon sm fill-muted" />
+      <span>Dashboard</span>
+    </a>
+  );
+
   return (
     <div className={`sidebar-panel ${!isOpen ? "close" : ""}`}>
       <ul className="nav flex-column">
         <li className="nav-item">
-          <a href="/" className="nav-link active">
-            <span>Dashboard</span>
-          </a>
+          {isOpen ? (
+            dashboardIcon
+          ) : (
+            <Tooltip placement="right" overlay={dashboardIcon}>
+              Dashboard
+            </Tooltip>
+          )}
         </li>
       </ul>
     </div>
