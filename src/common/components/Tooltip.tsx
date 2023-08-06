d3d6a0ff2b23c5cc97193010a21d0dbbe72f82a4
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-06 21:52:58 +0530

    position logic added

--- src/common/components/Tooltip.tsx
+++ src/common/components/Tooltip.tsx
@@ -4,10 +4,11 @@ import { FC, ReactNode, useRef, ReactElement, useEffect } from "react";
 interface TooltipProps {
   overlay: ReactElement<any> | ReactNode;
   children: ReactNode;
+  placement: "right" | "bottom";
 }
 
 const Tooltip: FC<TooltipProps> = (props) => {
-  const { overlay, children } = props;
+  const { overlay, placement, children } = props;
 
   const overlayRef = useRef<HTMLDivElement>(null);
   const tooltipRef = useRef<HTMLDivElement>(null);
@@ -21,14 +22,40 @@ const Tooltip: FC<TooltipProps> = (props) => {
 
   const handleMouseOver = () => {
     if (overlayRef.current && tooltipRef.current && arrowRef.current) {
-      const { left, width, bottom } = overlayRef?.current?.getBoundingClientRect();
-      const { width: tooltipWidth } = tooltipRef?.current?.getBoundingClientRect();
-      const { width: arrowWidth } = arrowRef?.current?.getBoundingClientRect();
-      const widthCenter = Math.round((left - tooltipWidth / 2 + width / 2) * 100) / 100;
+      const { left, width, bottom, top, height } = overlayRef?.current?.getBoundingClientRect();
+      const { width: tooltipWidth, height: tooltipHeight } = tooltipRef?.current?.getBoundingClientRect();
+      const { width: arrowWidth, height: arrowHeight } = arrowRef?.current?.getBoundingClientRect();
       tooltipRef.current?.classList.add("show");
-      tooltipRef.current.style.transform = `translate( ${widthCenter}px, ${bottom}px)`;
-      arrowRef.current.style.transform = `translate( ${tooltipWidth / 2 - arrowWidth / 2}px, 0px)`;
       tooltipRef.current.style.visibility = `unset`;
+
+      const dimensions = {
+        el: 0,
+        et: 0,
+        al: 0,
+        at: 0,
+      };
+
+      switch (placement) {
+        case "right": {
+          dimensions.el = left + width;
+          dimensions.et = top - (tooltipHeight / 2 - height / 2);
+          dimensions.al = 0;
+          dimensions.at = arrowHeight / 2;
+          break;
+        }
+        case "bottom":
+        default: {
+          const widthCenter = Math.round((left - tooltipWidth / 2 + width / 2) * 100) / 100;
+          dimensions.el = widthCenter;
+          dimensions.et = bottom;
+          dimensions.al = tooltipWidth / 2 - arrowWidth / 2;
+          dimensions.at = 0;
+          break;
+        }
+      }
+
+      tooltipRef.current.style.transform = `translate( ${dimensions.el}px, ${dimensions.et}px)`;
+      arrowRef.current.style.transform = `translate( ${dimensions.al}px, ${dimensions.at}px)`;
     }
   };
 
@@ -46,7 +73,7 @@ const Tooltip: FC<TooltipProps> = (props) => {
         className="tooltip bs-tooltip-auto fade show"
         role="tooltip"
         id="tooltip534701"
-        data-popper-placement="bottom"
+        data-popper-placement={placement}
         style={{
           position: "absolute",
           inset: "0px auto auto 0px",
