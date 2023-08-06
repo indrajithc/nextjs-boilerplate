21c59234ec292936b262b04e2642876156dc01d7
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-06 19:13:23 +0530

    types added

--- src/common/lib/types.d.ts
+++ src/common/lib/types.d.ts
@@ -1,3 +1,15 @@
 export interface IconProps {
   className?: string;
 }
+
+export interface RootState {
+  isOpenSideBar: Boolean;
+}
+
+export interface RootProviderProps extends RootState {
+  updateOpenSideBar: (status: Boolean) => void;
+}
+
+export interface ReactChildren {
+  children: React.ReactNode;
+}
