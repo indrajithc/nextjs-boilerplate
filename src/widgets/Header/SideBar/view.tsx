fbb4a08dddc544d46917051dee48699fdef0bd8d
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-07 11:44:16 +0530

    dynamic menu render

--- src/widgets/Header/SideBar/view.tsx
+++ src/widgets/Header/SideBar/view.tsx
@@ -1,53 +1,45 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
 import Tooltip from "@components/Tooltip";
 import Link from "@components/Link";
 
-import IconDashboard from "@icons/IconDashboard";
-import IconSearchHistory from "@icons/IconSearchHistory";
+type MenuItem = {
+  id: string;
+  href: string;
+  label: string;
+  icon?: ReactNode;
+  active: boolean;
+};
 
 interface SideBarViewProps {
   isOpen: Boolean;
+  menuItems: MenuItem[];
 }
 
 const SideBarView: FC<SideBarViewProps> = (props) => {
-  const { isOpen } = props;
+  const { isOpen, menuItems } = props;
 
-  const dashboardIcon = (
-    <Link href="/" className="nav-link active">
-      <IconDashboard className="icon sm fill-muted" />
-      <span>Dashboard</span>
-    </Link>
-  );
-
-  const searchHistoryIcon = (
-    <Link href="/" className="nav-link">
-      <IconSearchHistory className="icon sm fill-muted" />
-      <span>Search History</span>
+  const getMenuItem = (menuItem: MenuItem, isActive: boolean) => (
+    <Link href={menuItem.href} className={`nav-link${isActive ? " active" : ""}`}>
+      {menuItem.icon}
+      <span>{menuItem.label}</span>
     </Link>
   );
 
   return (
     <div className={`sidebar-panel ${!isOpen ? "close" : ""}`}>
       <ul className="nav flex-column">
-        <li className="nav-item">
-          {isOpen ? (
-            dashboardIcon
-          ) : (
-            <Tooltip placement="right" overlay={dashboardIcon}>
-              Dashboard
-            </Tooltip>
-          )}
-        </li>
-        <li className="nav-item">
-          {isOpen ? (
-            searchHistoryIcon
-          ) : (
-            <Tooltip placement="right" overlay={searchHistoryIcon}>
-              Search History
-            </Tooltip>
-          )}
-        </li>
+        {menuItems.map((menuItem, index) => (
+          <li className="nav-item " key={`${menuItem.id || index + 1}`}>
+            {isOpen ? (
+              getMenuItem(menuItem, menuItem.active)
+            ) : (
+              <Tooltip placement="right" overlay={getMenuItem(menuItem, menuItem.active)}>
+                {menuItem.label}
+              </Tooltip>
+            )}
+          </li>
+        ))}
       </ul>
     </div>
   );
