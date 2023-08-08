650d28d1d6a248316149a90b54b24a975c68a3cb
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-08 17:38:30 +0530

    edit product component

---
+++ src/widgets/Product/EditProduct/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { FC } from "react";
+
+import EditProductView from "./view";
+
+interface EditProductProps {}
+
+const EditProduct: FC<EditProductProps> = ({}) => {
+  return <EditProductView />;
+};
+
+export default EditProduct;
