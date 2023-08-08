d5365092937f827a2c3b25a4d7e699beef7e3487
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-08 15:12:05 +0530

    product list page

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 
 import Breadcrumb from "@components/Breadcrumb";
 import Footer from "@widgets/Footer";
+import { ProductList } from "@widgets/Product";
 
 interface ProductPageProps {}
 
@@ -25,7 +26,18 @@ const ProductPage: FC<ProductPageProps> = () => {
       <div className="title-panel">
         <div className="breadcrumb-panel">
           <Breadcrumb items={breadcrumbItems} />
+          <div className="row">
+            <div className="col">
+              <h3>Products</h3>
+            </div>
+            <div className="col-auto">
+              <button className="btn btn-primary">Add New</button>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="content-panel">
+        <ProductList />
       </div>
       <Footer />
     </main>
