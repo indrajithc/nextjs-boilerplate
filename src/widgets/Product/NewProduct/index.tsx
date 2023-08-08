fd8be67f30174524c68af54e91618ac4ed5b8656
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-08 17:17:23 +0530

    NewProduct component

---
+++ src/widgets/Product/NewProduct/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { FC } from "react";
+
+import NewProductView from "./view";
+
+interface NewProductProps {}
+
+const NewProduct: FC<NewProductProps> = ({}) => {
+  return <NewProductView />;
+};
+
+export default NewProduct;
