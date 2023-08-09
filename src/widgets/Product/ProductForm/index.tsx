166fe1738f25dcd8f408db10ed0db40cdf756c30
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-09 12:37:30 +0530

    common product component

---
+++ src/widgets/Product/ProductForm/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { FC } from "react";
+
+import ProductFormView from "./view";
+
+interface ProductFormProps {}
+
+const ProductForm: FC<ProductFormProps> = ({}) => {
+  return <ProductFormView />;
+};
+
+export default ProductForm;
