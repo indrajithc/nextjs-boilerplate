2ba7e53aa6fd94b2dbc1dd1fef91b52dcc1071c3
Indrajith C <indrajith.chandran@mozanta.com>
2023-08-18 12:02:44 +0530

    removed sample widgets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { FC } from "react";
 
-import SampleWidgetOne from "@widgets/SampleWidgetOne";
-import SampleWidgetTwo from "@widgets/SampleWidgetTwo";
-import SampleWidgetThree, { SampleWidgetComponent } from "@widgets/SampleWidgetThree";
 import Footer from "@widgets/Footer";
 
 interface HomePageProps {}
@@ -11,10 +8,6 @@ const HomePage: FC<HomePageProps> = () => {
   return (
     <main className="main-content-panel">
       Home page
-      <SampleWidgetOne />
-      <SampleWidgetTwo />
-      <SampleWidgetThree />
-      <SampleWidgetComponent />
       <Footer />
     </main>
   );
