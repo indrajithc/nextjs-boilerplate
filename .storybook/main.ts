b3918505bb0e46aa89c10a0c1e2415f0d5d1734c
Indrajith C <indrajith.chandran@mozanta.com>
2023-10-04 15:41:45 +0530

    perf: storybook latest

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,14 @@ import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-onboarding", "@storybook/addon-interactions", "storybook-dark-mode"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    "storybook-dark-mode",
+    "@storybook/addon-mdx-gfm"
+  ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
